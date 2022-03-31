8b49e8420761781b46f6c072adb38907d7942ed4
Vladlen Shaparev <shve@outlook.com>
2022-03-31 16:31:44 +0700

    Add project files.

---
+++ AskNumbers.cs
@@ -0,0 +1,64 @@
+﻿namespace ThreeNumbers
+{
+    internal static class AskNumbers
+    {
+        internal static (int minValue, int maxValue, uint decimalPlace)? Ask()
+        {
+            (int minValue, int maxValue, uint decimalPlace) answer;
+            string userAnswer;
+            do
+            {
+                Console.WriteLine("Enter first border number. Or type \"EXIT\" to terminate process.");
+                userAnswer = Console.ReadLine();
+                if (userAnswer.ToLower() == "exit")
+                {
+                    return null;
+                }
+                if (int.TryParse(userAnswer, out answer.minValue))
+                {
+                    break;
+                }
+                Console.WriteLine("Can't parse entered string.");
+            }
+            while (true);
+
+            do
+            {
+                Console.WriteLine("Enter second bordernumber. Or type \"EXIT\" to terminate process.");
+                userAnswer = Console.ReadLine();
+                if (userAnswer.ToLower() == "exit")
+                {
+                    return null;
+                }
+                if (int.TryParse(userAnswer, out answer.maxValue))
+                {
+                    if (answer.minValue > answer.maxValue)
+                    {
+                        (answer.maxValue, answer.minValue) = (answer.minValue, answer.maxValue);
+                    }
+                    break;
+                }
+                Console.WriteLine("Can't parse entered string.");
+            }
+            while (true);
+
+            do
+            {
+                Console.WriteLine("Enter amount of digits after decimal divider. Or type \"EXIT\" to terminate process.");
+                userAnswer = Console.ReadLine();
+                if (userAnswer.ToLower() == "exit")
+                {
+                    return null;
+                }
+                if (uint.TryParse(userAnswer, out answer.decimalPlace))
+                {
+                    break;
+                }
+                Console.WriteLine("Can't parse entered string.");
+            }
+            while (true);
+
+            return answer;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿using ThreeNumbers;
+
+var answer = AskNumbers.Ask();
+if (answer == null)
+{
+    return;
+}
+
+var randomizer = new Random();
+var firstNumber = randomizer.Next(answer.Value.minValue, answer.Value.maxValue + 1);
+Console.WriteLine($"First number = {firstNumber}");
+
+var secondNumber = randomizer.Next(answer.Value.minValue, answer.Value.maxValue + 1);
+Console.WriteLine($"Second number = {secondNumber}");
+
+var thirdNumber = randomizer.Next(answer.Value.minValue, answer.Value.maxValue + 1);
+Console.WriteLine($"Third number = {thirdNumber}");
+
+var midpointPosition = (int)answer.Value.decimalPlace;
+var mean = (double)(firstNumber + secondNumber + thirdNumber) / 3;
+Console.WriteLine($"Algebraic mean = {Math.Round(mean, midpointPosition)}");
+
+mean = Math.Pow(firstNumber * secondNumber * thirdNumber, (double)1 / 3);
+Console.WriteLine($"Geometrical mean = {Math.Round(mean, midpointPosition)}");
+
+if (firstNumber != 0)
+{
+    mean = (double)secondNumber / firstNumber;
+    Console.WriteLine($"Division of second number by first number = {Math.Round(mean, midpointPosition)}");
+    mean = (double)thirdNumber / firstNumber;
+    Console.WriteLine($"Division of third number by first number = {Math.Round(mean, midpointPosition)}");
+}
+if (secondNumber != 0)
+{
+    mean = (double)firstNumber / secondNumber;
+    Console.WriteLine($"Division of first nubmer by second number = {Math.Round(mean, midpointPosition)}");
+    mean = (double)thirdNumber / secondNumber;
+    Console.WriteLine($"Division of third number by second number = {Math.Round(mean, midpointPosition)}");
+}
+if (thirdNumber != 0)
+{
+    mean = (double)firstNumber / thirdNumber;
+    Console.WriteLine($"Division of first number by third number = {Math.Round(mean, midpointPosition)}");
+
+    mean = (double)secondNumber / thirdNumber;
+    Console.WriteLine($"Division of second number by third number = {Math.Round(mean, midpointPosition)}");
+}
+
+/*
+ * access to tupple fields problem
+ */
